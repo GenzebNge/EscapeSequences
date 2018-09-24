3c27715133ef27de0a833694024c454a4877d3cc
GBTC440004UR <GBTC440004UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-09-24 15:04:58 -0400

    Initial commit

---
+++ src/escapeSequences/Main.java
@@ -0,0 +1,15 @@
+package escapeSequences;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("\b********" + "*" + "\n" + "* " + "\t" + "\t"+ "*"  + "\n" + "*" + "\t" + "\t"+ "*" + "\n" + "*" + "\t" + "\t"+ "*"  + "\n" + "*" + "\t" + "\t"+ "*" + "\n********");
+
+        System.out.println("  ***" + "\n" + " *" + "\t" + " *" + "\n" + "*" + "\t " + " *" + "\n" + "*" + "\t " + " *" + "\n" + " *" + "\t " + "*" + "\n  ***");
+
+        System.out.println("   *" + "\n" +" * "+"\t" + "*" + "\n" + " * " + "\t" + " *" + "\n" + "  *");
+
+
+    }
+}
